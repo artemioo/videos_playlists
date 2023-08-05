8f60c6de1f68cf0c9ef130ce81bd0427b897456b
artemioo <artemryb1@gmail.com>
2023-08-05 12:28:04 +0300

    add signup validation

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import pathlib
-
-from fastapi import FastAPI, Request
+import json
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
-
+from pydantic.error_wrappers import ValidationError
+from .users.schemas import UserSignupSchema
 from . import db
 from app.users.models import User
 from .config import get_settings
@@ -38,8 +39,42 @@ def homepage(request: Request):
 
 @app.get("/login", response_class=HTMLResponse)
 def login_get_view(request: Request):
-
     return templates.TemplateResponse('auth/login.html', {'request': request})
+
+
+@app.post("/login", response_class=HTMLResponse)
+def login_post_view(request: Request,
+                    email: str = Form(...),
+                    password: str = Form(...)):
+    return templates.TemplateResponse('auth/login.html', {'request': request})
+
+
+@app.get("/signup", response_class=HTMLResponse)
+def login_get_view(request: Request):
+    return templates.TemplateResponse('auth/signup.html', {'request': request})
+
+
+@app.post("/signup", response_class=HTMLResponse)
+def signup_post_view(request: Request,
+                    email: str = Form(...),
+                    password: str = Form(...),
+                    password_confirm: str = Form(...)):
+    data = {}
+    errors = []
+    error_str = ""
+    try:
+        cleaned_data = UserSignupSchema(email=email, password=password, password_confirm=password_confirm)
+        data = cleaned_data.dict()
+    except ValidationError as e:
+        error_str = e.json()
+    try:
+        errors = json.loads(error_str)
+    except Exception:
+        errors = [{'loc': 'non_field_error', 'msg': 'Unknown error'}]
+    return templates.TemplateResponse('auth/signup.html',
+                                      {'request': request,
+                                       'data': data,
+                                       'errors': errors})
 
 
 @app.get('/users')
