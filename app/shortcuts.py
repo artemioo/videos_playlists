38d4b702aa99287233f19a98a586cd5f1470a0ec
artemioo <artemryb1@gmail.com>
2023-08-07 10:57:15 +0300

    add cookie with httponly to render

--- app/shortcuts.py
+++ app/shortcuts.py
@@ -8,12 +8,18 @@ settings = get_settings()
 templates = Jinja2Templates(directory=str(settings.TEMPLATES_DIR))
 
 
-def render(request, template_name, context={}, status_code: int=200):
+def render(request, template_name, context={}, status_code: int=200, cookies: dict = {}):
     ctx = context.copy()
     ctx.update({'request': request})
-
     t = templates.get_template(template_name)
     html_str = t.render(ctx)
     response = HTMLResponse('something', status_code=status_code)
+    response.set_cookie(key='darkmode', value=1)
+    if len(cookies.keys()):
+        # set httponly cookies
+        for k, v in cookies.items():
+            response.set_cookie(key=k, value=v, httponly=True)
+    # delete cookies
+    # for key in request.cookies.keys():
+    #      response.delete_cookie(key)
     return response
-    # return templates.TemplateResponse(template_name, ctx)
