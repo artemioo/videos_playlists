83fb0768cb4d3303f64118677adb0d3a3438e3bf
artemioo <artemryb1@gmail.com>
2023-08-09 11:56:03 +0300

    add extract_url func and extend add_video method

--- app/videos/models.py
+++ app/videos/models.py
@@ -3,6 +3,9 @@ from app.config import get_settings
 from cassandra.cqlengine import columns
 from cassandra.cqlengine.models import Model
 
+from app.users.models import User
+from app.videos.extractors import extract_video_id
+
 settings = get_settings()
 
 
@@ -10,7 +13,7 @@ class Video(Model):
     __keyspace__ = settings.ASTRADB_KEYSPACE
     host_id = columns.Text(primary_key=True)  # YouTube
     db_id = columns.UUID(primary_key=True, default=uuid.uuid1) # UUID1
-    host_servise = columns.Text(default='youtube')
+    host_service = columns.Text(default='youtube')
     url = columns.Text()  # secure
     user_id = columns.UUID()       # owner
     title = columns.Text()
@@ -20,8 +23,18 @@ class Video(Model):
         return self.__repr__()
 
     def __repr__(self):
-        return f'Video(email={self.email}, user_id={self.user_id})'
+        return f'Video(host_id={self.host_id}, host_service={self.host_service})'
 
     @staticmethod
     def add_video(url, user_id=None):
-        pass
+        host_id = extract_video_id(url)  #vnSVYUJrAZU
+        if host_id is None:
+            raise Exception('Invalid YouTube Video URL')
+        user_exists = User.check_exists(user_id)
+        if user_exists is None:
+            raise Exception('Invalid user_id')
+        q = Video.objects.allow_filtering().filter(host_id=host_id, user_id=user_id)
+        if q.count() != 0:
+            raise Exception('Video already added')
+        return Video.create(host_id=host_id, user_id=user_id, url=url)
+# https://www.youtube.com/watch?v=vnSVYUJrAZU
