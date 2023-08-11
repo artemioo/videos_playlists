9e0313e4dc23cb8a0a8d9f7ab1d4836c502d69cf
artemioo <artemryb1@gmail.com>
2023-08-11 14:03:07 +0300

    [upd] YouTube JavaScript Player

--- app/main.py
+++ app/main.py
@@ -121,3 +121,10 @@ def signup_post_view(request: Request,
 async def users_list_view():
     q = User.objects.all().limit(10)
     return list(q)
+
+
+@app.post('/watch-event')
+def watch_event_view(request: Request, data:dict):
+    print(data)
+    print(request.user.is_authenticated)
+    return {'working': True }
