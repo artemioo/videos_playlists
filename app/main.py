cd736b7be10df80fe2e2faa56d4805b64a8584ff
artemioo <artemryb1@gmail.com>
2023-08-12 13:14:24 +0300

    add more Watch Event data

--- app/main.py
+++ app/main.py
@@ -129,10 +129,12 @@ async def users_list_view():
 def watch_event_view(request: Request, data: dict):
     print(data)
     if request.user.is_authenticated:
-        WatchEvent.objects.create(host_id=data.get('videoId'),
+        obj = WatchEvent.objects.create(host_id=data.get('videoId'),
                                   user_id=request.user.username,
-                                  start_time=0,
+                                  start_time=data.get('startTime'),
                                   end_time=data.get('currentTime'),
-                                  duration=500,
-                                  complete=False)
+                                  duration=data.get('duration'),
+                                  complete=data.get('complete')
+                                  )
+        print(obj)
     return {'working': True}
