81008ee9dc4e2ca2d017899951906527a3ba5e97
artemioo <artemryb1@gmail.com>
2023-08-15 13:13:08 +0300

    connect HMTX and create 'add video htmx form'

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Form
+from fastapi import APIRouter, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from app.users.decorators import login_required
 from app.shortcuts import render, redirect, get_object_or_404
@@ -13,6 +13,10 @@ router = APIRouter(
 )
 
 
+def is_htmx(request: Request):
+    return request.headers.get('hx-request') == 'true'
+
+
 @router.get('/', response_class=HTMLResponse)
 def video_list_view(request: Request):
     q = Video.objects.all().limit(100)
@@ -24,7 +28,9 @@ def video_list_view(request: Request):
 
 @router.get('/create', response_class=HTMLResponse)
 @login_required
-def video_create_view(request: Request):
+def video_create_view(request: Request, is_htmx=Depends(is_htmx)):
+    if is_htmx:
+        return render(request, "videos/htmx/create.html", {})
     return render(request, "videos/create.html", {})
 
 
